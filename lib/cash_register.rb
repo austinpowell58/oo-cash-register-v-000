d8813789ad463594c6ef2ffc0aaac22a0af982c0
Austin Powell <austinpowell58@gmail.com>
2018-08-18 17:34:55 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,32 @@
 
+class CashRegister
+  
+  attr_accessor :discount, :total
+  
+  def initialize(employee_discount = nil)
+    @discount = employee_discount
+    @total = 0 
+  end 
+  
+  def add_item(item, quantity = nil,  price)
+    if quantity != nil
+      @total += (quantity * price)
+    else
+      @total += price
+    end 
+  end
+  
+  def apply_discount()
+    if @discount != nil
+      price = @total * (1 - @discount)
+      "After the discount, the total comes to #{price}."
+      @total = price
+    else
+      "There is no discount to apply."
+    end 
+  end 
+    
+  
+  
+  
+end 
